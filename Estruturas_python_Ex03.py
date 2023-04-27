5951a0d03dd5bbd27039b0cdf74eb35e22569751
Bellafarias <pereira.isabellafarias@gmail.com>
2023-04-27 09:07:51 -0300

    Foi realizado a adição da lista das unidades de armazenamento, e prints introdutórios para a maior compreenssão do usuário, adicionado também as constantes para a realização dos cálculos futuramente, e as variáveis sobre a adição de valores.

--- Estruturas_python_Ex03.py
+++ Estruturas_python_Ex03.py
@@ -1,14 +1,10 @@
 medidas_armazenamento =["byte", "kilobyte", "megabyte", "gigabyte", "terabyte", "petabyte"]
-valor_padrao = 1024
+PRINCIPAL= 1024
+VALOR_BYTE_BIT = 8
 
-def dadinhos():
-    print("Preencha alguns dados com relação a lista a seguir:", medidas_armazenamento)
-    medidas_nominhos = input("Escolha uma unidade de armazenamento você gostaria de converter:")
-    while medidas_nominhos not in medidas_armazenamento:
-        medidas_nominhos = input ("Unidade não encontrada, tente novamente:")
-    medidas_nomes = input("Escolha uma unidade de armazenamento para que o valor seja convertido:")
-    while medidas_nomes not in medidas_armazenamento:
-        medidas_nomes = input ("Unidade não encontrada, tente novamente:")
-    unidades_para_converter = int(input("Insira um valor à ser convertido:"))
-    return medidas_nomes, medidas_nominhos, unidades_para_converter
+print("Preencha alguns dados com relação a lista de unidades de armazenamento a seguir:")
+print("\nLista de unidades de armazenamento:", medidas_armazenamento)
 
+medidas_iniciais = input("Escolha uma unidade de armazenamento você gostaria de converter:")
+medidas_final = input("Escolha uma unidade de armazenamento para que o valor seja convertido:")
+unidades_para_converter = int(input("Insira um valor à ser convertido:"))
