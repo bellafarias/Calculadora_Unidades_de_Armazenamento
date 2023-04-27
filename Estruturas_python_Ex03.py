c22e4949ded2a2f1424bd00b309a0e96006c7b73
Bellafarias <pereira.isabellafarias@gmail.com>
2023-04-27 09:19:46 -0300

    Foi realizado a adição do processo de validação das unidades, para caso inserido uma unidade que não consta na lista ocorra a chance do usuário tente novemente o processo de inserir as unidades.

--- Estruturas_python_Ex03.py
+++ Estruturas_python_Ex03.py
@@ -6,5 +6,15 @@ print("Preencha alguns dados com relação a lista de unidades de armazenamento 
 print("\nLista de unidades de armazenamento:", medidas_armazenamento)
 
 medidas_iniciais = input("Escolha uma unidade de armazenamento você gostaria de converter:")
+while medidas_iniciais not in medidas_armazenamento:
+    medidas_iniciais = input ("Unidade não encontrada, tente novamente:")
+else:
+    origem_marota = medidas_armazenamento.index(medidas_iniciais)
+
 medidas_final = input("Escolha uma unidade de armazenamento para que o valor seja convertido:")
+while medidas_final not in medidas_armazenamento:
+    medidas_final = input ("Unidade não encontrada, tente novamente:")
+else:
+    destino_maroto = medidas_armazenamento.index(medidas_final)
+
 unidades_para_converter = int(input("Insira um valor à ser convertido:"))
