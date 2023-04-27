a5176bd90689dbdfccaed0f491ea5d31293ee2b0
Bellafarias <pereira.isabellafarias@gmail.com>
2023-04-27 09:51:12 -0300

    Foi realizado a adição do cálculo de armazenamento das unidades multiplas por 1024, foi adicionado também a função dadinhos com o cálculo de armazenamento e adição das variáveis destino e origem que colaboram para o desenvolvimento do cálculo, e adicionado a medida bit na lista de armazenamentos, sendo a função chamada no final para que ocorra a visualização correta, e a alteração do print das medidas iniciais e finais, tornando mais fácil a sua compreensão.

--- Estruturas_python_Ex03.py
+++ Estruturas_python_Ex03.py
@@ -1,20 +1,31 @@
-medidas_armazenamento =["byte", "kilobyte", "megabyte", "gigabyte", "terabyte", "petabyte"]
+medidas_armazenamento =["bit", "byte", "kilobyte", "megabyte", "gigabyte", "terabyte", "petabyte"]
 PRINCIPAL= 1024
 VALOR_BYTE_BIT = 8
 
 print("Preencha alguns dados com relação a lista de unidades de armazenamento a seguir:")
 print("\nLista de unidades de armazenamento:", medidas_armazenamento)
 
-medidas_iniciais = input("Escolha uma unidade de armazenamento você gostaria de converter:")
+medidas_iniciais = input("Escolha uma unidade de armazenamento você gostaria de converter em relação a lista anterior:")
 while medidas_iniciais not in medidas_armazenamento:
     medidas_iniciais = input ("Unidade não encontrada, tente novamente:")
 else:
     origem_marota = medidas_armazenamento.index(medidas_iniciais)
 
-medidas_final = input("Escolha uma unidade de armazenamento para que o valor seja convertido:")
+medidas_final = input("Escolha uma unidade de armazenamento para que o valor seja convertidoem relação a lista anterior:")
 while medidas_final not in medidas_armazenamento:
     medidas_final = input ("Unidade não encontrada, tente novamente:")
 else:
     destino_maroto = medidas_armazenamento.index(medidas_final)
 
 unidades_para_converter = int(input("Insira um valor à ser convertido:"))
+
+def dadinhos_convert():
+    if origem_marota < destino_maroto:
+        tamanho = destino_maroto - origem_marota
+        resultado = unidades_para_converter / (PRINCIPAL**tamanho)
+    else:
+        tamanho = destino_maroto - origem_marota
+        resultado = unidades_para_converter * (PRINCIPAL**tamanho)
+    print ("Seu resultado é:", resultado, str(medidas_final))
+
+dadinhos_convert()
